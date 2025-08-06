95552210a210677f8966ea04c9b6870a928057ba
Pier-Luc Richard <84756667+pirichar@users.noreply.github.com>
2025-08-06 14:29:29 -0400

    second project added and strated

---
+++ 02-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  standalone: true,
+  styleUrl: './user-input.component.css',
+  imports: [FormsModule],
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    // Emit the values as numbers
+    // the + transforms the string into a number
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
+  }
+}
